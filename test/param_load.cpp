3c46659d4fefb7fef3ef84389d7490b4904317eb
Atsushi Watanabe <atsushi.w@ieee.org>
2019-05-09 11:14:20 +0000

    Rebase from 'debian/lunar/ypspur'

--- test/param_load.cpp
+++ test/param_load.cpp
@@ -1,3 +1,23 @@
+// Copyright (c) 2010-2016 The YP-Spur Authors, except where otherwise indicated.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,14 +27,16 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif
+#endif  // __cplusplus
 
 #include <param.h>
 #include <ypparam.h>
 
 #ifdef __cplusplus
 }
-#endif
+#endif  // __cplusplus
+
+extern Parameters g_param;
 
 class ParamLoadTest : public ::testing::Test
 {
@@ -32,6 +54,7 @@ protected:
 bool skipKeys(const int key)
 {
   if (static_cast<YPSpur_param>(key) == YP_PARAM_VERSION ||
+      static_cast<YPSpur_param>(key) == YP_PARAM_ENCODER_DENOMINATOR ||
       static_cast<YPSpur_param>(key) == YP_PARAM_RADIUS_R ||
       static_cast<YPSpur_param>(key) == YP_PARAM_RADIUS_L)
     return true;
@@ -42,7 +65,7 @@ TEST_F(ParamLoadTest, LeftRightCommon)
 {
   char param_names[YP_PARAM_NUM][32] = YP_PARAM_NAME;
 
-  fprintf(fp, "VERSION: %f\n", YP_PARAM_REQUIRED_VERSION);
+  fprintf(fp, "VERSION: %f\n", YP_PARAM_SUPPORTED_VERSION);
   for (int i = 0; i < YP_PARAM_NUM; i++)
   {
     if (param_names[i][0] == '_' || skipKeys(i))
@@ -52,6 +75,7 @@ TEST_F(ParamLoadTest, LeftRightCommon)
   fflush(fp);
   rewind(fp);
   ASSERT_GT(set_paramptr(fp), 0);
+  ASSERT_EQ(g_param.num_motor_enable, 2);
 
   for (int i = 0; i < YP_PARAM_NUM; i++)
   {
@@ -66,7 +90,7 @@ TEST_F(ParamLoadTest, LeftRightDifferent)
 {
   char param_names[YP_PARAM_NUM][32] = YP_PARAM_NAME;
 
-  fprintf(fp, "VERSION: %f\n", YP_PARAM_REQUIRED_VERSION);
+  fprintf(fp, "VERSION: %f\n", YP_PARAM_SUPPORTED_VERSION);
   for (int i = 0; i < YP_PARAM_NUM; i++)
   {
     if (param_names[i][0] == '_' || skipKeys(i))
@@ -77,6 +101,7 @@ TEST_F(ParamLoadTest, LeftRightDifferent)
   fflush(fp);
   rewind(fp);
   ASSERT_GT(set_paramptr(fp), 0);
+  ASSERT_EQ(g_param.num_motor_enable, 2);
 
   for (int i = 0; i < YP_PARAM_NUM; i++)
   {
@@ -91,7 +116,7 @@ TEST_F(ParamLoadTest, NoNewline)
 {
   char param_names[YP_PARAM_NUM][32] = YP_PARAM_NAME;
 
-  fprintf(fp, "VERSION: %f", YP_PARAM_REQUIRED_VERSION);
+  fprintf(fp, "VERSION: %f", YP_PARAM_SUPPORTED_VERSION);
   for (int i = 0; i < YP_PARAM_NUM; i++)
   {
     if (param_names[i][0] == '_' || skipKeys(i))
@@ -101,6 +126,7 @@ TEST_F(ParamLoadTest, NoNewline)
   fflush(fp);
   rewind(fp);
   ASSERT_GT(set_paramptr(fp), 0);
+  ASSERT_EQ(g_param.num_motor_enable, 2);
 
   for (int i = 0; i < YP_PARAM_NUM; i++)
   {
@@ -109,6 +135,44 @@ TEST_F(ParamLoadTest, NoNewline)
     ASSERT_DOUBLE_EQ(p(static_cast<YPSpur_param>(i), MOTOR_RIGHT), i * 0.1 + 10.0);
     ASSERT_DOUBLE_EQ(p(static_cast<YPSpur_param>(i), MOTOR_LEFT), i * 0.1 + 10.0);
   }
+}
+
+TEST_F(ParamLoadTest, EncoderDenominatorUnsupported)
+{
+  char param_names[YP_PARAM_NUM][32] = YP_PARAM_NAME;
+
+  fprintf(fp, "VERSION: 4.0\n");
+  fprintf(fp, "ENCODER_DENOMINATOR: 5.0\n");
+  for (int i = 0; i < YP_PARAM_NUM; i++)
+  {
+    if (param_names[i][0] == '_' || skipKeys(i))
+      continue;
+    fprintf(fp, "%s: %f\n", param_names[i], i * 0.1 + 10.0);
+  }
+  fflush(fp);
+  rewind(fp);
+  ASSERT_EQ(set_paramptr(fp), 0);
+}
+
+TEST_F(ParamLoadTest, EncoderDenominatorSupported)
+{
+  char param_names[YP_PARAM_NUM][32] = YP_PARAM_NAME;
+
+  fprintf(fp, "VERSION: %f\n", YP_PARAM_SUPPORTED_VERSION);
+  fprintf(fp, "ENCODER_DENOMINATOR: 5.0\n");
+  for (int i = 0; i < YP_PARAM_NUM; i++)
+  {
+    if (param_names[i][0] == '_' || skipKeys(i))
+      continue;
+    fprintf(fp, "%s: %f\n", param_names[i], i * 0.1 + 10.0);
+  }
+  fflush(fp);
+  rewind(fp);
+  ASSERT_GT(set_paramptr(fp), 0);
+  ASSERT_EQ(g_param.num_motor_enable, 2);
+
+  ASSERT_DOUBLE_EQ(p(static_cast<YPSpur_param>(YP_PARAM_ENCODER_DENOMINATOR), MOTOR_RIGHT), 5.0);
+  ASSERT_DOUBLE_EQ(p(static_cast<YPSpur_param>(YP_PARAM_ENCODER_DENOMINATOR), MOTOR_LEFT), 5.0);
 }
 
 int main(int argc, char **argv)
