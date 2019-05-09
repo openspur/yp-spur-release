d30391e8558b25c0753f7b2c5fbd585cd016f6a7
Atsushi Watanabe <atsushi.w@ieee.org>
2019-05-09 11:13:57 +0000

    Rebase from 'debian/lunar/ypspur'

--- src/command_aux.c
+++ src/command_aux.c
@@ -18,7 +18,62 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#ifndef YPSPUR_COORDINATOR_H
-#define YPSPUR_COORDINATOR_H
+#include <math.h>
+#include <stdio.h>
+#include <strings.h>
+#include <unistd.h>
 
-#endif  // YPSPUR_COORDINATOR_H
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <time.h>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif  // HAVE_CONFIG_H
+
+/* ボディパラメータ */
+#include <shvel-param.h>
+
+/* yp-spur用 */
+#include <serial.h>
+#include <adinput.h>
+#include <param.h>
+#include <odometry.h>
+
+/* ライブラリ用 */
+#include <ypspur.h>
+#include <cartesian2d.h>
+
+void get_ad_com(double *data, double *resdata)
+{
+  int num;
+
+  num = (int)data[0];
+  resdata[0] = get_addata(num);
+}
+
+void set_io_dir_com(double *data, double *resdata)
+{
+  int num;
+
+  num = (int)data[0];
+  parameter_set(PARAM_io_dir, 0, num);
+}
+
+void set_io_data_com(double *data, double *resdata)
+{
+  int num;
+
+  num = (int)data[0];
+  parameter_set(PARAM_io_data, 0, num);
+}
+
+void get_error_state_com(double *data, double *resdata)
+{
+  int num = (int)data[0];
+  ErrorStatePtr err = get_error_state_ptr();
+
+  resdata[0] = err->state[num];
+  resdata[1] = err->time[num];
+}
